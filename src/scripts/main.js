78b12c334b13f8196403b1ef23b48a3d718212ab
Maxwyame06 <maxwyame06@gmail.com>
2022-06-06 21:59:27 +0200

    ajout bon liens vers css et js -v3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@
 
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 let tl = gsap.timeline({
     scrollTrigger:{
       trigger: '.mockup-landing',
